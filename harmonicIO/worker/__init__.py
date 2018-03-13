d4ee47e516915e73f59627aae95dd6f80d7e0a15
Snapple49 <oliver-dot-stein@hotmail.com>
2018-03-13 11:42:03 +0100

    removed unecessary components

--- harmonicIO/worker/__init__.py
+++
@@ -1,9 +0,0 @@
-import importlib
-
-falcon_spec = importlib.util.find_spec("falcon")
-if falcon_spec is None:
-    raise Exception("Falcon module has not been installed.")
-
-docker_spec = importlib.util.find_spec("docker")
-if docker_spec is None:
-    raise Exception("Docker module has not been installed.")
