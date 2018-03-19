9376a12eb2ba2b30f63d8e82dffff68ad907a6f8
Snapple49 <oliver-dot-stein@hotmail.com>
2018-03-19 20:09:27 +0100

    progress on simulator, cleaned up some stuff

--- simulator.py
+++ simulator.py
@@ -2,8 +2,11 @@ import threading
 import requests
 import json
 import time
-
+import os
+from harmonicIO.stream_connector import stream_connector
             
+
+
 
 def read_cfg_json():
     path = "./config.json"
@@ -15,7 +18,7 @@ def read_cfg_json():
             print("Could not read config file: \n{}".format(e.msg))
     return ret
 
-def data_collector(dict_storage):
+def data_collector(dict_storage, logger):
     """
     Gather data from master node
 
@@ -29,6 +32,7 @@ def data_collector(dict_storage):
         resp = requests.get(url)
         if resp.status_code == 200:
             dict_storage[int(time.clock())] = resp.text
+            logger.log_event("Got data from master!")
 
 def event_thread(events):
     time.clock()
@@ -39,32 +43,77 @@ def event_thread(events):
                 pass
 
 
+
 class Event:
 
-    def __init__(self, params, cfg):
+    def __init__(self, params, cfg, logger):
         self.periodic = params.get('periodic')
         self.time = params.get('time')
         self.container = params.get('c_name')
         self.type = params.get('type')
         self.ip = cfg.get('master_ip')
+        self.port = cfg.get('master_port')
         
     def send_host_request(self, num, volatile):
         url = "http://{}:8080/jobRequest?token=None&type=new_job".format(self.ip) 
         req_data = {"c_name" : self.container, "num" : num, "volatile" : volatile}
         resp = requests.post(url, data=req_data)
         return resp.status_code
-        
+
+    def read_data_from_file(self, path):
+        func_data = bytearray()
+
+        with open(path, 'rb') as f:
+            lines = f.readlines()
+
+            for line in lines:
+                func_data += line
+
+        return func_data
+
     def send_stream_request(self):
+        sc = stream_connector.StreamConnector(self.ip, self.port , token="None", std_idle_time=1)
+
+        
+        if sc.is_master_alive():
+            data = self.read_data_from_file("./sample_image.bmp")
+            data_to_send = sc.get_data_container()
+            data_to_send += data
+
+            sc.send_data(self.container, "ubuntu", data_to_send)
+        
         
 
 
+class Logger:
+    def __init__(self):
+        self.logname = "simulator_log_{}".format(time.strftime("%Y-%m-%d__%H_%M", time.localtime()))
+        self.starttime = time.clock()
+        self.i = 1
+        while self.logname in os.listdir():
+            self.logname += "({})".format(self.i)
+            self.i+=1
+            
+        self.logname += ".log"
+
+        logfile = open(self.logname, 'x')
+        logfile.close()
+
+    def log_event(self, message):
+        try:
+            logfile = open(self.logname, 'a')
+            logfile.write(time.strftime("%H:%M:%S - ", time.localtime()) + " --- " + message + '\n')
+            logfile.close()
+        except:
+            print("Could not write to log!")
 
 class Simulator:
-    
+
     def __init__(self, duration):
         self.duration = duration
         self.events = []
         self.system_output = {}
+        self.logger = Logger()
         
         self.sim_config = read_cfg_json()
         self.create_events(self.sim_config.get('events'))
@@ -79,10 +128,15 @@ class Simulator:
 
     def create_events(self, cfg_events):
         for item in cfg_events:
-            self.events.append(Event(item, self.sim_config))
+            self.events.append(Event(item, self.sim_config, self.logger))
         
     def start_sim(self):
+        starting_time = time.clock()
+        self.logger.log_event("Starting simulation!")
         self.data_col_thread.start()
+        time.sleep(self.duration)
+        self.logger.log_event("Simulation finished, time elapsed: {} seconds".format(time.clock() - starting_time))
+
         
 
 
